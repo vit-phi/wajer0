56c89f4b8dca3b5528ff96c476734d3c130b5c80
vit-phi <mosnox120@gmail.com>
2025-01-29 14:11:53 +0700

    mss

--- src/layouts/LayoutUser.jsx
+++ src/layouts/LayoutUser.jsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import MainNav from '../components/MainNav';
-import ToggleMode from '../utils/ToggleMode'; // Import ปุ่มเปลี่ยนโหมด
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import MainNav from '../components/MainNav'
 
 const LayoutUser = () => {
     return (
         <div>
-            {/* Navbar */}
             <MainNav />
 
-            {/* ปุ่มเปลี่ยนโหมด */}
-            <div className="text-right p-3">
-                <ToggleMode />
-            </div>
-
-            {/* Main Content */}
             <main className='h-full px-4 mt-2 mx-auto'>
                 <Outlet />
             </main>
-        </div>
-    );
-};
 
-export default LayoutUser;
+        </div>
+    )
+}
+
+export default LayoutUser
