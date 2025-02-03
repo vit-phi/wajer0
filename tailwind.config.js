90b06ca30f8c3961934f4db4326d0600368f8b70
vit-phi <mosnox120@gmail.com>
2025-02-03 14:51:37 +0700

    mos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class", // ✅ เปิดใช้งาน Dark Mode
+  
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
