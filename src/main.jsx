db9fb9c356576eb5b5e6449b5e16aab9b8c6d941
vit-phi <mosnox120@gmail.com>
2025-02-04 12:45:27 +0700

    13

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { ThemeProvider } from "./context/ThemeContext"; // ✅ เพิ่ม ThemeProvider
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
