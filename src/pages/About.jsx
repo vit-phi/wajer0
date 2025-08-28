510ba59bdfdb6d716482c0bb2a219a7f33e89bf0
vit-phi <mosnox120@gmail.com>
2025-08-28 12:34:39 +0700

    mos999

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,55 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const AboutMe = () => {
   const students = [
-   
-    {
-      name: "นาย วิศณุ พิมานรัมย์ 6652300796",
-      name: "นาย กนกพล ศรีโสพล 6652300699",
-      
-      image: "/images/abc.jpg"
-      
-    },
-    
+    { name: "วิศณุ พิมานรัมย์ 6652300796" },
+    { name: "กนกพล ศรีโสพล 6652300699" }
   ];
-
-  // ใช้ useState เพื่อเก็บสถานะของการซูม
-  const [isZoomed, setIsZoomed] = useState(false);
-
-  const handleZoom = (image) => {
-    setIsZoomed(!isZoomed);
-    // เพิ่มฟังก์ชันเพื่อซูมเข้าออกที่ภาพ
-  };
 
   return (
     <section id="about-me" className="bg-gray-100 py-16">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-semibold text-gray-800 mb-8">About Me</h2>
+        <h2 className="text-3xl font-semibold text-gray-800 mb-8">จัดทำโดย</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {students.map((student, index) => (
-            <div key={index} className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <div
+              key={index}
+              className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+            >
               <h3 className="text-xl font-medium text-gray-800">{student.name}</h3>
-              <p className="text-sm text-gray-600 mt-2"> {student.studentId}</p>
-              <div className="mt-4">
-                <p className="text-sm text-gray-600">
-                  GitHub: <a href={student.github} className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">{student.github}</a>
-                </p>
-                <p className="text-sm text-gray-600 mt-2">
-                  Resume: <a href={student.resume} className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">{student.resume}</a>
-                </p>
-              </div>
-
-              {/* ภาพที่สามารถซูมได้ */}
-              <div 
-                className={`mt-4 relative ${isZoomed ? "zoomed-in" : ""}`}
-                onClick={() => handleZoom(student.image)}
-              >
-                <img
-                  className="rounded-md w-full h-full object-cover transition-transform duration-300"
-                  src={student.image}
-                  alt={`Image of ${student.name}`}
-                />
-              </div>
             </div>
           ))}
         </div>
